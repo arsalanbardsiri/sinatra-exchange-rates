09f8745a7eaff8850cac6a06bdb3253dcaad317c
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-16 19:48:39 +0000

    From To

--- app.rb
+++ app.rb
@@ -2,6 +2,43 @@ require "sinatra"
 require "sinatra/reloader"
 require "dotenv/load"
 require "http"
+
+EXCHANGE = ENV.fetch("EXCHANGE_KEY")
+EXCHANGE_URL = "https://api.exchangerate.host/list?access_key=#{EXCHANGE}"
+CONVERT_URL  = "https://api.exchangerate.host/convert"
+
+helpers do
+  def fetch_currencies
+    raw = HTTP.get(EXCHANGE_URL).to_s
+    data = JSON.parse(raw)
+    data.fetch("currencies") # Hash "CODE" => "Name"
+  end
+end
+
+get("/") do
+  @list_data = fetch_currencies
+  erb :index
+end
+
+get("/:from") do
+  @from = params[:from].upcase
+
+  @list_data = fetch_currencies
+
+  unless @list_data.key?(@from)
+    halt 404, "Unknown currency code: #{@from}"
+  end
+
+  #since 1 == 1 why not remove it?
+  #1 AED equals 1 AED.
+  @targets = @list_data.keys.reject { |k| k == @from }.sort
+
+  erb :from
+end
+
+get("/:from/:to") do
+  #example result
+  #1 AED equals 1 AED.
 =begin
 https://api.exchangerate.host/convert
     ? access_key = EXCHANGE_KEY
@@ -9,29 +46,47 @@ https://api.exchangerate.host/convert
     & to = GBP
     & amount = 10
 =end
-EXCHANGE = ENV.fetch("EXCHANGE_KEY")
-EXCHANGE_URL = "https://api.exchangerate.host/list?access_key=#{EXCHANGE}"
 
-get("/") do
-  raw = HTTP.get(EXCHANGE_URL).to_s
-  formated = JSON.parse(raw)
-  @list_data = formated.fetch("currencies")
-  erb :index
-end
-
-get("/:from") do
   @from = params[:from].upcase
+  @to = params[:to].upcase
 
-  raw = HTTP.get(EXCHANGE_URL).to_s
-  formated = JSON.parse(raw)
-  @list_data = formated.fetch("currencies")
-
+  @list_data = fetch_currencies
+  
   unless @list_data.key?(@from)
     halt 404, "Unknown currency code: #{@from}"
   end
+  
+  unless @list_data.key?(@to)
+    halt 404, "Unknown currency code: #{@to}"
+  end
 
-  #since 1 == 1 why not remove it?
-  @targets = @list_data.keys.reject { |k| k == @from }.sort
+    raw  = HTTP.get(CONVERT_URL, params: {
+    access_key: EXCHANGE,
+    from: @from,
+    to:   @to,
+    amount: 1
+    }).to_s
+    
+    json = JSON.parse(raw)
+=begin
+{
+  "success": true,
+  "terms": "https://currencylayer.com/terms",
+  "privacy": "https://currencylayer.com/privacy",
+  "query": {
+    "from": "USD",
+    "to": "GBP",
+    "amount": 1
+  },
+  "info": {
+    "timestamp": 1758050644,
+    "quote": 0.73215
+  },
+  "result": 0.73215
+}
+=end
+  @rate = json["result"] || json.dig("info", "rate") || json.dig("info", "quote")
+  halt 502, "Conversion unavailable right now" unless @rate
 
-  erb :from
+  erb :convert
 end
