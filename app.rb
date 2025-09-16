568b9d1af2b59f76cd57c2c258b84987f67866cb
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-16 18:45:43 +0000

    PicoCSS add, List item href for next route

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 require "dotenv/load"
 require "http"
-
+=begin
+https://api.exchangerate.host/convert
+    ? access_key = EXCHANGE_KEY
+    & from = USD
+    & to = GBP
+    & amount = 10
+=end
 exchange = ENV.fetch("EXCHANGE_KEY")
 exchange_rate = "https://api.exchangerate.host/list?access_key=#{exchange}"
 
