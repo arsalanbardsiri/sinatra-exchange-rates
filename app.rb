6d23dddfa0965ba95d5e94181352d067d1dd3ec7
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-16 19:04:16 +0000

    Global Vars,Front-End From Views,Back-End from Logic

--- app.rb
+++ app.rb
@@ -9,12 +9,28 @@ https://api.exchangerate.host/convert
     & to = GBP
     & amount = 10
 =end
-exchange = ENV.fetch("EXCHANGE_KEY")
-exchange_rate = "https://api.exchangerate.host/list?access_key=#{exchange}"
+EXCHANGE = ENV.fetch("EXCHANGE_KEY")
+EXCHANGE_URL = "https://api.exchangerate.host/list?access_key=#{EXCHANGE}"
 
 get("/") do
-  raw = HTTP.get(exchange_rate).to_s
+  raw = HTTP.get(EXCHANGE_URL).to_s
   formated = JSON.parse(raw)
   @list_data = formated.fetch("currencies")
   erb :index
+end
+
+get("/:from") do
+  @from = params[:from].upcase
+
+  raw = HTTP.get(EXCHANGE_URL).to_s
+  formated = JSON.parse(raw)
+  @list_data = formated.fetch("currencies")
+
+  unless @list_data.key?(@from)
+    halt 404, "Unknown currency code: #{@from}"
+  end
+
+  @targets = @list_data.keys.reject { |k| k == @from }.sort
+
+  erb :from
 end
